c5aa310e9e667a7959fa2f0205fdeaada4a9f098
Musango <musangow@gmail.com>
2022-07-17 22:53:55 +0200

    create ForgotPasswordView.tsx

---
+++ src/unauth-views/ForgotPasswordView.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { RouteComponentProps } from '@reach/router'
+
+const ForgotPasswordView = ({}: RouteComponentProps) => {
+  return <div>Forgot password</div>
+}
+export default ForgotPasswordView
