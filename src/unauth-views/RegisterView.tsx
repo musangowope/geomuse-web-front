d4e6ff0b31d8aee011a945f784759ad50183a64b
Musango <musangow@gmail.com>
2022-07-17 22:51:05 +0200

    create RegisterVeiw.tsx

---
+++ src/unauth-views/RegisterView.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { RouteComponentProps } from '@reach/router'
+
+const RegisterView = ({}: RouteComponentProps) => {
+  return <div>Register</div>
+}
+
+export default RegisterView
