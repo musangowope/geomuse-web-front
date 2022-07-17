2797746f9048010c3365a62403bec3911f0e1159
Musango <musangow@gmail.com>
2022-07-17 22:46:47 +0200

    create DashboardView/index.tsx

---
+++ src/authed-views/DashboardView/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { RouteComponentProps, useLocation } from '@reach/router'
+import { ChildrenType } from '../../utils/common-types-and-interfaces'
+import MusicMap from '../../components/MusicMap'
+import { useAppDispatch } from '../../redux/store'
+import { getPlaylistsAsync } from '../../redux/slices/playlistsSlice'
+
+type DashboardViewType = RouteComponentProps & ChildrenType
+
+const DashboardView = ({ children }: DashboardViewType) => {
+  const location = useLocation()
+  const dispatch = useAppDispatch()
+  React.useEffect(() => {
+    dispatch(getPlaylistsAsync())
+  }, [dispatch])
+
+  const renderChildren = location.pathname !== '/'
+  return (
+    <div className='tw-relative tw-h-full'>
+      <MusicMap />
+      {renderChildren ? (
+        <div className='tw-absolute tw-top-0 tw-left-0 tw-h-full tw-w-full'>{children}</div>
+      ) : null}
+    </div>
+  )
+}
+
+export default DashboardView
