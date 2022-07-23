d9a2a93342624fa4990b17cc71a76811417dfd8c
Musango <musangow@gmail.com>
2022-07-23 16:47:48 +0200

    common-types-and-interfaces.ts: remove ChildrenType

--- src/utils/common-types-and-interfaces.ts
+++ src/utils/common-types-and-interfaces.ts
@@ -1,12 +1,6 @@
-import React from 'react'
-
 export type BaseRequestType = {
   loading: boolean
   success: boolean
   failed: boolean
   error: any
-}
-
-export type ChildrenType = {
-  children: React.ReactNode | string
 }
