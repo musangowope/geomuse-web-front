38683dc11af6129ca8adb044c416e00d8ee8c88b
Musango <musangow@gmail.com>
2022-07-23 16:52:39 +0200

    SearchView.tsx: use MusicSearch to search for songs and render TrackCards

--- src/authed-views/DashboardView/SearchView.tsx
+++ src/authed-views/DashboardView/SearchView.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { navigate, RouteComponentProps } from '@reach/router'
-import { useGetSearchResultsQuery } from '../../redux/queries/search'
-import GenreCard from '../../components/GenreCard'
-import TrackCard from '../../components/TrackCard'
 
-const genres = [
+import MusicSearch from '../../components/redux-components/MusicSearch'
+import TrackCard from '../../components/simple-components/TrackCard'
+import { TrackType } from '../../redux/slices/playlistsSlice'
+
+const genreFilters = [
   {
     label: 'Ambient',
     value: 'ambient',
@@ -59,72 +60,27 @@ const genres = [
   },
 ]
 
-export interface iTrackType {
-  image?: string
-  name?: string
-  artist?: string
-}
-
 const SearchView = ({}: RouteComponentProps) => {
-  const [q, setQ] = React.useState<string>('')
-  const [genresFilter, setGenresFilter] = React.useState<string[]>([])
-  const [query, setQuery] = React.useState('')
-
-  const onQChange = (e: { target: { value: React.SetStateAction<string> } }) => {
-    setQ(e.target.value)
-  }
-
-  React.useEffect(() => {
-    if (q || genresFilter.length) {
-      const queryData: {
-        [key: string]: string
-      } = {
-        ...(q ? { q } : {}),
-        ...(genresFilter.length ? { genres: genresFilter.join('+') } : {}),
-      }
-      const string = Object.keys(queryData)
-        .map((key) => {
-          return `${key}=${queryData[key]}`
-        })
-        .join('&')
-      setQuery(string)
-    }
-  }, [q, genresFilter.length, genresFilter])
-
-  const onGenreClick = (genre: string) => {
-    setGenresFilter((prev) => [...prev, genre])
-  }
-
-  const { data = [], isLoading } = useGetSearchResultsQuery(query)
-
   return (
     <div className='search-view'>
       <div className='tw-container tw-mx-auto'>
-        <div>
-          <input placeholder='Search tracks' onChange={onQChange} />
-        </div>
-        <div className='tw-flex tw-gap-2'>
-          {genres.map((genre: { label: string; value: string }, index) => (
-            <GenreCard key={index} onClick={() => onGenreClick(genre.value)} title={genre.label} />
-          ))}
-        </div>
-        {isLoading ? <div>LOADING</div> : null}
-
-        <div className='tw-gap-2 tw-grid tw-grid-cols-4'>
-          {data.map((track: iTrackType, index: React.Key | null | undefined) => (
-            <TrackCard
-              key={index}
-              image={track.image}
-              name={track.name}
-              artist={track.artist}
-              onMenuClick={() =>
-                navigate('/playlists', {
-                  state: { track },
-                })
-              }
-            />
-          ))}
-        </div>
+        <MusicSearch genreFilters={genreFilters}>
+          {({ data }: { data: TrackType[] }) =>
+            data.map((track: TrackType, index: React.Key | null | undefined) => (
+              <TrackCard
+                key={index}
+                image={track.image}
+                name={track.name}
+                artist={track.artist}
+                onMenuClick={() =>
+                  navigate('/playlists', {
+                    state: { track },
+                  })
+                }
+              />
+            ))
+          }
+        </MusicSearch>
       </div>
     </div>
   )
