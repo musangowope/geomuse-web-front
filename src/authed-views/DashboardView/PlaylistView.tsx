821942653ec6c2ad89c6b83a2a4bc7c5f47ac0ba
Musango <musangow@gmail.com>
2022-07-23 16:45:05 +0200

    PlaylistView.tsx: add cb function to onPlaylistChange and dispatch updatePlaylistAsync

--- src/authed-views/DashboardView/PlaylistView.tsx
+++ src/authed-views/DashboardView/PlaylistView.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { RouteComponentProps, useParams } from '@reach/router'
-import PlaylistForm from '../../components/PlaylistForm'
+import PlaylistForm from '../../components/simple-components/PlaylistForm'
 import { useSelector } from 'react-redux'
-import { iReduxState } from '../../redux/store'
-import { PlaylistType } from '../../redux/slices/playlistsSlice'
+import { iReduxState, useAppDispatch } from '../../redux/store'
+import { PlaylistType, updatePlaylistAsync } from '../../redux/slices/playlistsSlice'
 
 const PlaylistView = ({}: RouteComponentProps) => {
   const { id = '' } = useParams()
   const playlists = useSelector((state: iReduxState) => state.playlists)
-  const playlist = playlists.playlists.find((playlist) => playlist.id === id) as PlaylistType
+  const playlist: PlaylistType | undefined = playlists.playlists.find(
+    (playlist) => playlist.id === id,
+  )
+
+  const dispatch = useAppDispatch()
+
+  const updatePlaylist = async (playlist: PlaylistType) => {
+    dispatch(updatePlaylistAsync(playlist))
+  }
+
   return (
     <div className='search-view'>
-      <PlaylistForm playlist={playlist} />
+      {playlist ? <PlaylistForm playlist={playlist} onPlaylistChange={updatePlaylist} /> : null}
     </div>
   )
 }
