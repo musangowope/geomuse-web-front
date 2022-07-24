b8bfe8dd0bb3319eef17a9cea7f6c064b4707331
Musango <musangow@gmail.com>
2022-07-24 14:15:20 +0200

    MusicMap.tsx: map track markers connected to redux

--- src/components/redux-components/MusicMap.tsx
+++ src/components/redux-components/MusicMap.tsx
@@ -1,68 +1,171 @@
-import React from 'react'
-
-import GoogleMapReact from 'google-map-react'
+import * as React from 'react'
+import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import { addPlaylistAsync, PlaylistType } from '../../redux/slices/playlistsSlice'
 import { iReduxState, useAppDispatch } from '../../redux/store'
 import { useSelector } from 'react-redux'
-import { addPlaylistAsync } from '../../redux/slices/playlistsSlice'
 
-const AnyReactComponent = ({ text = '' }) => <div>{text}</div>
+const render = (status: Status) => {
+  return <h1>{status}</h1>
+}
 
-export default function MusicMap() {
-  const [coordinates, setCoordinates] = React.useState<{
-    lat: number | null
-    lng: number | null
-  }>({
-    lat: null,
-    lng: null,
+const MusicMap: React.FC = () => {
+  const [zoom, setZoom] = React.useState(15) // initial zoom
+  const [mapInstance, setMapInstance] = React.useState<any>()
+  const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
+    lat: 0,
+    lng: 0,
   })
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setCoordinates({
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
+  React.useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCenter({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        })
       })
-    })
-  }
+    }
+  }, [])
 
   const dispatch = useAppDispatch()
   const { playlists } = useSelector((state: iReduxState) => state.playlists)
-
-  const onMapClick = (e: { lat: number; lng: number }) => {
+  const onClick = (e: google.maps.MapMouseEvent) => {
+    const lat = e.latLng?.lat() || 0
+    const lng = e.latLng?.lng() || 0
     dispatch(
       addPlaylistAsync({
-        name: `${e.lat} ${e.lng}`,
+        name: `${lat} ${lng}`,
         tracks: [],
-        lat: e.lat,
-        lng: e.lng,
+        lat,
+        lng,
         dateCreated: new Date(),
         isDraft: true,
       }),
     )
   }
 
-  return (
-    // Important! Always set the container height explicitly
+  const onIdle = (m: google.maps.Map) => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    setZoom(m.getZoom()!)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    setCenter(m.getCenter()?.toJSON())
+  }
 
-    <div style={{ height: '100vh', width: '100%' }}>
-      {coordinates.lat && coordinates.lng ? (
-        <GoogleMapReact
-          draggable
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
-          defaultCenter={{
-            lng: coordinates.lng,
-            lat: coordinates.lat,
-          }}
-          defaultZoom={15}
-          onClick={onMapClick}
+  const apiKey: string = process.env.GOOGLE_MAPS_API_KEY || ''
+
+
+  return (
+    <div style={{ display: 'flex', height: '100%' }}>
+       <Wrapper apiKey={apiKey} render={render} callback={() => setMapInstance(window.google)}>
+        <Map
+          center={center}
+          onClick={onClick}
+          onIdle={onIdle}
+          zoom={zoom}
+          style={{ flexGrow: '1', height: '100%' }}
         >
-          {playlists.map((playlist) => (
-            <AnyReactComponent key={playlist.id} text={playlist.name} />
-          ))}
-        </GoogleMapReact>
-      ) : null}
+           {playlists.map((playlist, i) =>
+            mapInstance && playlist.lat && playlist.lng && google ? (
+              <Marker
+                key={i}
+                position={
+                  new mapInstance.maps.LatLng({
+                    lat: playlist.lat,
+                    lng: playlist.lng,
+                  })
+                }
+              />
+            ) : null,
+           )}
+        </Map>
+       </Wrapper>
     </div>
   )
 }
+interface MapProps extends google.maps.MapOptions {
+  style: { [key: string]: string }
+  onClick?: (e: google.maps.MapMouseEvent) => void
+  onIdle?: (map: google.maps.Map) => void
+  children?: React.ReactNode
+}
+
+const Map: React.FC<MapProps> = ({ onClick, onIdle, children, style, ...options }) => {
+  const ref = React.useRef<HTMLDivElement>(null)
+  const [map, setMap] = React.useState<google.maps.Map>()
+
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(new window.google.maps.Map(ref.current, {}))
+    }
+  }, [ref, map])
+
+  // because React does not do deep comparisons, a custom hook is used
+  // see discussion in https://github.com/googlemaps/js-samples/issues/946
+  useDeepCompareEffectForMaps(() => {
+    if (map) {
+      map.setOptions(options)
+    }
+  }, [map, options])
+
+  React.useEffect(() => {
+    if (map) {
+      ;['click', 'idle'].forEach((eventName) => google.maps.event.clearListeners(map, eventName))
+
+      if (onClick) {
+        map.addListener('click', onClick)
+      }
+
+      if (onIdle) {
+        map.addListener('idle', () => onIdle(map))
+      }
+    }
+  }, [map, onClick, onIdle])
+
+  return (
+    <>
+      <div ref={ref} style={style} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          // set the map prop on the child component
+          return React.cloneElement(child, { map })
+        }
+      })}
+    </>
+  )
+}
+
+interface iMarker extends google.maps.MarkerOptions {
+  playlist?: PlaylistType
+}
+
+const Marker: React.FC<iMarker> = (options) => {
+  const [marker, setMarker] = React.useState<google.maps.Marker>()
+
+  React.useEffect(() => {
+    if (!marker) {
+      setMarker(new google.maps.Marker())
+    }
+
+    // remove marker from map on unmount
+    return () => {
+      if (marker) {
+        marker.setMap(null)
+      }
+    }
+  }, [marker])
+
+  React.useEffect(() => {
+    if (marker) {
+      marker.setOptions(options)
+    }
+  }, [marker, options])
+
+  return null
+}
+
+function useDeepCompareEffectForMaps(callback: React.EffectCallback, dependencies: any[]) {
+  React.useEffect(callback, [callback])
+}
+
+export default MusicMap
